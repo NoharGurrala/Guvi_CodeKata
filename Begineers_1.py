e6ff2ff6dc4c7104221e49d496ddbcce485156bc
Gurrala Nohar Reddy <36082931+NoharGurrala@users.noreply.github.com>
2019-04-04 18:46:57 +0530

    Update Begineers_1.py

--- Begineers_1.py
+++ Begineers_1.py
@@ -1,7 +1,10 @@
-n = input()
-if n > 0:
-    print ('Positive')
-elif n < 0:
-    print ('Negative')
-else:
-    print ('Zero')
+try:
+    n = input()
+    if n > 0:
+      print ('Positive')
+    elif n < 0:
+      print ('Negative')
+    else:
+      print ('Zero')
+except:
+    print ('Invalid Input')

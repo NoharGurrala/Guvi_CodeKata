02515416885f2551d9c0b4c38ead32cae63607a1
Gurrala Nohar Reddy <36082931+NoharGurrala@users.noreply.github.com>
2019-07-02 14:18:01 +0530

    Update Pro_15.py

--- Pro_15.py
+++ Pro_15.py
@@ -1,5 +1,12 @@
 n = input()
 array = map(int,raw_input().split())
 array.sort()
+ans=[]
 for i in range(0,n):
-    print array[(n-1)-i]
+    ans.append(bin(array[i])[2:])
+ans.sort()
+main = []
+for i in range(0,n):
+    main.append(int(ans[i],2))
+for i in  range(0,n):
+    print(main[(n-1)-i])

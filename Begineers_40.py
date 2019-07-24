9ec3169e15ad593b8dc986344c3dd6b7e1ac3e3a
Gurrala Nohar Reddy <36082931+NoharGurrala@users.noreply.github.com>
2019-07-24 22:37:21 +0530

    Update Begineers_40.py

--- Begineers_40.py
+++ Begineers_40.py
@@ -1,8 +1,13 @@
 n = input()
 i = 1
 j = 1
-print i,j,
-for k in range(0,n-2):
+if(n == 1):
+    print i,
+elif(n == 2):
+    print i,j,
+else:
+    print i,j,
+    for k in range(0,n-2):
     s = i + j
     print s,
     i = j
